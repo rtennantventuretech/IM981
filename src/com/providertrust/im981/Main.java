cec75b6f13d62859c2a3c86e4a7f2b2d58e549bf
Ying Tan <ytan@venturetech.net>
2017-08-24 10:57:16 -0500

    commit the readOnly connection as well.

--- src/com/providertrust/im981/Main.java
+++ src/com/providertrust/im981/Main.java
@@ -163,7 +163,7 @@ public class Main
         throws SQLException
     {
         try (ResultSet gridAddressIdResult = addressLineStmt.executeQuery(//WHERE addresslines_id = 429854
-            "select distinct addresslines_id from ptgrid.addresslines_aud WHERE addresslines_id"
+            "select distinct addresslines_id from ptgrid.addresslines_aud "
                 + " GROUP BY addresslines_id, rev having count(rev) > 1 ORDER BY addresslines_id"))
         {
             while (gridAddressIdResult.next())
